45e92d9f07db08c079d61a1370be3cd92095db99
yassineedev <yassine.boussif18@gmail.com>
2023-11-28 15:37:15 +0100

    commit 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for chars1 in range(0,10):
-	for chars2 in range(chars1 + 1,10):
-		if chars1 == 8 and chars2 == 9:
-			print("{}{}".format(chars1, chars2))
-		else:
-			print("{}{}".format(char1, chars2), end=", ")
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
