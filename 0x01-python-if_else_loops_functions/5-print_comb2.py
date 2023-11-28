cfb972dd18a973d6db335184aaf0647d89499560
yassineedev <yassine.boussif18@gmail.com>
2023-11-28 15:25:46 +0100

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:d} = 0x{:x}".format(i, i))
+for i in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
