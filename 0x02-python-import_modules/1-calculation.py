80e407e065de9aa6d83c2de5ddea342385584c05
yassineedev <yassine.boussif18@gmail.com>
2023-11-30 17:30:42 +0100

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))    
